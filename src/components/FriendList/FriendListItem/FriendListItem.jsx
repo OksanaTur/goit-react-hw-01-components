782b960a84a9aa9f5aca921b72659934b0a7a1d1
OksanaTur <ksyturkevych@gmail.com>
2023-02-20 18:50:27 +0400

    fixed

--- src/components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem/FriendListItem.jsx
@@ -6,7 +6,7 @@ export const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
   
     return (
         <>
-            <Status isOnline={isOnline===true}>{isOnline}</Status>
+            <Status isOnline={isOnline}>{isOnline}</Status>
         <Avatar src={avatar} alt="User avatar" width="48" />
             <Name>{name}</Name>
             </>
@@ -15,7 +15,7 @@ export const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
 
 FriendListItem.propTypes = {
     friend:  PropTypes.shape({
-            isOnline: PropTypes.oneOf('true', 'false').isRequired,
+            isOnline: PropTypes.bool.isRequired,
             avatar: PropTypes.string.isRequired,
             name: PropTypes.string.isRequired,
         }).isRequired,
